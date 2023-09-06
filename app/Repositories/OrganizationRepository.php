1454f03a5d0a764517d9f09419bd67dfcf021c6b
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2023-09-06 08:54:17 -0300

    feat: created the organization entity

---
+++ app/Repositories/OrganizationRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Organization;
+
+class SquadRepository extends AbstractRepository
+{
+    public function __construct(Organization $model)
+    {
+        $this->model = $model;
+    }
+
+    public function getAll(array $filterParams = [])
+    {
+        $query = $this->model->query();
+
+        $perPage = 5;
+        $page = 1;
+
+        if (isset($filterParams['searchParams']['name']) && !empty($filterParams['searchParams']['name'])) {
+            $name = $filterParams['searchParams']['name'];
+            $query->where('name', 'LIKE', "%$name%");
+        }
+
+        $this->setPaginationParameters($filterParams, $perPage, $page);
+
+        return $query->paginate($perPage, ['*'], 'page', $page);
+    }
+}
